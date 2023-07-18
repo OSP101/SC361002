f7ffc9ae62d6fe3d80bd9fa4be46960cb46a0f08
OSP101 <supphitan.pk@gmail.com>
2023-07-19 04:34:12 +0700

    update section 1

---
+++ LAB05/Section1/Lab05-03.py
@@ -0,0 +1,8 @@
+number = int(input("กรุณาป้อนตัวเลข: "))
+
+if number > 0:
+    print(number, "เป็นจำนวนเต็มบวก")
+elif number < 0:
+    print(number, "เป็นจำนวนเต็มลบ")
+else:
+    print(number, "เป็นศูนย์")
