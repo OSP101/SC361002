18f95a6b0c84b98024ad8a565e917a8fcdde146f
Supphitan Paksawad <supphitan.pk@gmail.com>
2023-08-27 17:07:21 +0700

    update lab09 sec4

---
+++ LAB09/Section 4/Lab09-2.py
@@ -0,0 +1,18 @@
+subject=[]
+score=[]
+num=1
+while True:
+    if num==6:
+        break
+    sub=str(input('กรอกวิชาที่ {} : '.format(num)))
+    subject.append(sub)
+    sco=float(input('กรอกคะแนน :'))
+    score.append(sco)
+    num=num+1
+s=min(score)
+S=max(score)
+sc=score.index(s)
+Sc=score.index(S)
+print("------------------------------------")
+print('คะแนนสูงสุด :',S,'| วิชา :',subject[Sc])
+print('คะแนนต่ำสุด :',s,'| วิชา :',subject[sc])
